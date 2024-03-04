d9687d63718d32beae441cc11cebef068f85fcd4
irsyadhamdi <144642758+irsyadhamdi@users.noreply.github.com>
2024-03-04 09:50:48 +0800

    Add files via upload

--- logout.php
+++ logout.php
@@ -1,7 +1,10 @@
 <?php
-   session_start();
-   
-   if(session_destroy()) {
-      header("Location: LogIn.php");
-   }
+require 'connection.php';
+require 'session.php';
+
+session_destroy();
 ?>
+<script type="text/javascript">
+	alert("Successfully logout!");
+	window.location = "login.php";
+</script>
